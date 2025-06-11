6a7a7aabc7be6f2ee5c62869e1463b3149c1549d
Ster <acetynne@gmail.com>
2025-06-11 23:32:46 +0300

    initialize

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const form =document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    form.addEventListener('submit',function(event){
+        event.preventDefault();
+
+    //  validate username
+
+    const username = document.getElementById('username').value.trim();
+    const email =document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+
+    let isValid = true ;
+    const message = [];
+
+    //  validate username
+
+    if(username.length <3){
+        isValid=false;
+        messages.push("Username must be at least 3 character long")
+    }
+    // email validation
+    if(!email.includes('@')||!email.includes('.')){
+        isValue=false;
+        messages.push("Please enter avalid email address.")
+    }
+
+    // password validate
+
+    if (password.length <8){
+        isValid=false;
+        messages.push("Password must be at least 8 characters long")
+    }
+
+    // display feedback
+    feedbackDiv.style.display='block';
+    if(isValid){
+        feedbackDiv.textContent="Registration Successful!"
+        feedbackDiv.style.color="#28a745"
+    }else{
+        feedbackDiv.textContent=messages.join('<br>')
+        feedbackDiv.style.color="#dc3545"
+    }
+
+    })
+})
